2648a4cc3de951020e7546f989ff4b934317741b
shemot <42626564+h8ed@users.noreply.github.com>
2021-05-15 20:06:35 -0500

    Update functions.el

--- lisp/functions.el
+++ lisp/functions.el
@@ -107,7 +107,7 @@
 (defun o/backward-word-active ()
   (interactive)
   (o/set-mark-if-inactive)
-  (kakoune-backward-same-syntax 1))
+  (forward-same-syntax -1))
 
 (defun o/forward-word-active ()
   (interactive)
@@ -312,5 +312,18 @@
       (current-buffer))
     '((side . bottom)))))
 (global-set-key (kbd "M-c") 'o/pop-shell)
+
+(defun xah-new-empty-buffer ()
+  "Create a new empty buffer.
+New buffer will be named “untitled” or “untitled<2>”, “untitled<3>”, etc.
+It returns the buffer (for elisp programing).
+URL `http://ergoemacs.org/emacs/emacs_new_empty_buffer.html'
+Version 2017-11-01"
+  (interactive)
+  (let (($buf (generate-new-buffer "untitled")))
+    (switch-to-buffer $buf)
+    (funcall initial-major-mode)
+    (setq buffer-offer-save t)
+    $buf))
 
 (provide 'functions)
